5dc31de6e124791a55619ca3174733f7cc6f9946
Matei Sîrbu <matei.sirbu@student.unitbv.ro>
2021-03-01 17:08:05 +0200

    Generated components and configured routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DessertsComponent } from './components/desserts/desserts.component';
+import { LoginComponent } from './components/login/login.component';
+import { UploadComponent } from './components/upload/upload.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'desserts', component: DessertsComponent },
+  { path: 'upload', component: UploadComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { UploadComponent } from './components/upload/upload.component';
+import { DessertsComponent } from './components/desserts/desserts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UploadComponent,
+    DessertsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
