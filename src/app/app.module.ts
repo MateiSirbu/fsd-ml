21a3009c325daf76db41dc8248f6c216c5925235
Matei Sîrbu <matei.sirbu@student.unitbv.ro>
2021-03-02 17:56:33 +0200

    Added sidenav buttons, miscellaneous aesthetic improvements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,28 +8,23 @@ import { LoginComponent } from './components/login/login.component';
 import { UploadComponent } from './components/upload/upload.component';
 import { DessertsComponent } from './components/desserts/desserts.component';
 import { HomeComponent } from './components/home/home.component';
-
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-
 import { ForgotComponent } from './components/forgot/forgot.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
-
-
-
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator'
 import { MatSortModule } from '@angular/material/sort';
 import { MatCheckboxModule } from '@angular/material/checkbox'
-import { MatExpansionModule } from '@angular/material/expansion'; 
-import {MatInputModule} from '@angular/material/input'
-import {FormsModule, ReactiveFormsModule} from '@angular/forms'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field';
-
-import {DessertListMockService } from './components/desserts/desserts-items.mock.service';
-
+import { DessertListMockService } from './components/desserts/desserts-items.mock.service';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -58,8 +53,9 @@ import {DessertListMockService } from './components/desserts/desserts-items.mock
     MatInputModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule
-
+    MatFormFieldModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [DessertListMockService],
   bootstrap: [AppComponent]
